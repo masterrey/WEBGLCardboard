21127c3d67ccfacfc119b80dde4b48eaefdcffba
Reinaldo Ramos <masterrey@gmail.com>
2023-09-01 22:15:49 -0300

    fix cardboardbug

--- Assets/Cardboard/Scripts/CardboardController.cs
+++ Assets/Cardboard/Scripts/CardboardController.cs
@@ -30,14 +30,14 @@ namespace TiltShift.Cardboard
         [Range(0.1f, 5f)] public float RenderTextureMultiplicator = 2f;
 
         public float ViewDPI => Util.GetDPI();
-       
 
 
-        private Material      _leftEyeMaterial;
-        private Material      _rightEyeMaterial;
-        private RenderTexture _leftEyeRT  => LeftEyeCam.targetTexture;
+
+        private Material _leftEyeMaterial;
+        private Material _rightEyeMaterial;
+        private RenderTexture _leftEyeRT => LeftEyeCam.targetTexture;
         private RenderTexture _rightEyeRT => RightEyeCam.targetTexture;
-        
+
         #region Prev orientation states.
 
         private bool _autorotateToLandscapeLeft;
@@ -59,8 +59,8 @@ namespace TiltShift.Cardboard
             "CgZIb21pZG8SDUhvbWlkbyAibWluaSIdhxZZPSW28309KhAAAEhCAABIQgAASEIAAEhCWAE1KVwPPToIexQuPs3MTD1QAGAC";
 
         private string _filePresetName = "cardboardPayload";
-        private int    ViewWidth  => Screen.width;
-        private int    ViewHeight => Screen.height;
+        private int ViewWidth => Screen.width;
+        private int ViewHeight => Screen.height;
 
         private float Ratio => ViewWidth / ViewHeight * Util.GetDPI();
 
@@ -71,7 +71,7 @@ namespace TiltShift.Cardboard
         {
             LoadLastQrCode();
 
-            PlayerPrefs.GetFloat("manualzoom", manualzoom);
+            manualzoom = PlayerPrefs.GetFloat("manualzoom", 1.4f);
         }
 
         private void OnEnable()
@@ -79,19 +79,19 @@ namespace TiltShift.Cardboard
 
             SubscribeButton();
 #if UNITY_PIPELINE_URP || UNITY_PIPELINE_HDRP
-// code for URP or HDRP
+            // code for URP or HDRP
             RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
 #endif
-            Application.targetFrameRate              =  60;
-            _screenTimeOut                           =  Screen.sleepTimeout;
-            Screen.sleepTimeout                      =  SleepTimeout.NeverSleep;
-            _autorotateToLandscapeLeft               =  Screen.autorotateToLandscapeLeft;
-            _autorotateToLandscapeRight              =  Screen.autorotateToLandscapeRight;
-            _autorotateToPortrait                    =  Screen.autorotateToPortrait;
-            _autorotateToPortraitUpSide              =  Screen.autorotateToPortraitUpsideDown;
-            Screen.autorotateToLandscapeLeft         =  Screen.autorotateToLandscapeRight     = true;
-            Screen.autorotateToPortrait              =  Screen.autorotateToPortraitUpsideDown = false;
-            Screen.orientation                       =  ScreenOrientation.LandscapeLeft;
+            Application.targetFrameRate = 60;
+            _screenTimeOut = Screen.sleepTimeout;
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            _autorotateToLandscapeLeft = Screen.autorotateToLandscapeLeft;
+            _autorotateToLandscapeRight = Screen.autorotateToLandscapeRight;
+            _autorotateToPortrait = Screen.autorotateToPortrait;
+            _autorotateToPortraitUpSide = Screen.autorotateToPortraitUpsideDown;
+            Screen.autorotateToLandscapeLeft = Screen.autorotateToLandscapeRight = true;
+            Screen.autorotateToPortrait = Screen.autorotateToPortraitUpsideDown = false;
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
 
             foreach (var obj in ObjectsForHidden)
             {
@@ -104,13 +104,13 @@ namespace TiltShift.Cardboard
         {
             UnSubscribeButton();
 #if UNITY_PIPELINE_URP || UNITY_PIPELINE_HDRP
-// code for URP or HDRP
+            // code for URP or HDRP
             RenderPipelineManager.endCameraRendering -= OnEndCameraRendering;
 #endif
-            Screen.sleepTimeout                   = _screenTimeOut;
-            Screen.autorotateToLandscapeLeft      = _autorotateToLandscapeLeft;
-            Screen.autorotateToLandscapeRight     = _autorotateToLandscapeRight;
-            Screen.autorotateToPortrait           = _autorotateToPortrait;
+            Screen.sleepTimeout = _screenTimeOut;
+            Screen.autorotateToLandscapeLeft = _autorotateToLandscapeLeft;
+            Screen.autorotateToLandscapeRight = _autorotateToLandscapeRight;
+            Screen.autorotateToPortrait = _autorotateToPortrait;
             Screen.autorotateToPortraitUpsideDown = _autorotateToPortraitUpSide;
 
             foreach (var obj in ObjectsForHidden)
@@ -118,32 +118,32 @@ namespace TiltShift.Cardboard
                 obj.SetActive(true);
             }
         }
-        
+
         private void LoadLastQrCode()
         {
-            
-            if (cardboardpayload!=null)
+
+            if (cardboardpayload != null)
             {
                 var payload = cardboardpayload.text;
                 _qrPayload = payload;
                 Debug.Log("Cardboard: QRCode loaded");
-               
-               
-            }
-             /*
-            try
-            {
 
-                var payload = File.ReadAllText(PathToSettings);
-                Debug.Log(PathToSettings);
-                _qrPayload = payload;
-                Debug.Log("Cardboard: QRCode loaded");
+
             }
-            catch (FileNotFoundException)
-            {
-                Debug.LogWarning($"Cardboard: Saved QRCode is not exist. ");
-            }
-            */
+            /*
+           try
+           {
+
+               var payload = File.ReadAllText(PathToSettings);
+               Debug.Log(PathToSettings);
+               _qrPayload = payload;
+               Debug.Log("Cardboard: QRCode loaded");
+           }
+           catch (FileNotFoundException)
+           {
+               Debug.LogWarning($"Cardboard: Saved QRCode is not exist. ");
+           }
+           */
             SetPayload(_qrPayload);
         }
 
@@ -181,10 +181,10 @@ namespace TiltShift.Cardboard
 
             BothCam.projectionMatrix = Matrix4x4.Ortho(-1, 1, -1, 1, -0.1f, 0.5f);
 
-            LeftEyeCam.transform.localPosition  = _lensDistortion.GetEyePosition(CardboardEye.Left);
+            LeftEyeCam.transform.localPosition = _lensDistortion.GetEyePosition(CardboardEye.Left);
             RightEyeCam.transform.localPosition = _lensDistortion.GetEyePosition(CardboardEye.Right);
 
-            LeftEyeCam.projectionMatrix  = _lensDistortion.GetProjectionMatrix(CardboardEye.Left);
+            LeftEyeCam.projectionMatrix = _lensDistortion.GetProjectionMatrix(CardboardEye.Left);
             RightEyeCam.projectionMatrix = _lensDistortion.GetProjectionMatrix(CardboardEye.Right);
 
             Debug.Log("Cardboard: Camera's initialized");
@@ -192,21 +192,21 @@ namespace TiltShift.Cardboard
 
         private void InitTargetTextures()
         {
-            var size = new Vector2(ViewWidth / 2f * 2f, ViewHeight ) ;
+            var size = new Vector2(ViewWidth / 2f * 2f, ViewHeight);
 
-            var leftRt  = new RenderTexture((int) size.x, (int) size.y, 24, GraphicsFormat.R8G8B8A8_UNorm, 2);
-            var rightRt = new RenderTexture((int) size.x, (int) size.y, 24, GraphicsFormat.R8G8B8A8_UNorm, 2);
+            var leftRt = new RenderTexture((int)size.x, (int)size.y, 24, GraphicsFormat.R8G8B8A8_UNorm, 2);
+            var rightRt = new RenderTexture((int)size.x, (int)size.y, 24, GraphicsFormat.R8G8B8A8_UNorm, 2);
 
             leftRt.antiAliasing = rightRt.antiAliasing = 2;
 
-            LeftEyeCam.targetTexture  = leftRt;
+            LeftEyeCam.targetTexture = leftRt;
             RightEyeCam.targetTexture = rightRt;
             Debug.Log("Cardboard: target initialized");
         }
 
         private void InitMaterials()
         {
-            _leftEyeMaterial  = new Material(Shader.Find(ShaderName));
+            _leftEyeMaterial = new Material(Shader.Find(ShaderName));
             _rightEyeMaterial = new Material(Shader.Find(ShaderName));
 
             Debug.Log("Cardboard: Materials initialized");
@@ -221,13 +221,13 @@ namespace TiltShift.Cardboard
             }
 
             UpdateSizes();
-           
+
         }
 
         private void UpdateSizes()
         {
             _prevRatio = Ratio;
-            _lensDistortion.UpdateSize(ViewWidth , ViewHeight , ViewDPI* manualZoom);
+            _lensDistortion.UpdateSize(ViewWidth, ViewHeight, ViewDPI * manualzoom);
             InitCameras();
 
             Debug.Log("Cardboard: Reinit graphics");
@@ -236,30 +236,27 @@ namespace TiltShift.Cardboard
         //using 2 fingers to change the zoom in and out in the manualzoom variable
 
         public float zoomSpeed = 0.1f; // The speed of the zoom
-        public float manualZoom=1; // The variable where the current zoom level is stored
 
-        private Vector2[] oldTouchPositions; // positions of fingers in previous frame
-        private Vector2[] newTouchPositions; // positions of fingers in current frame
-        private float oldTouchDistance; // distance between fingers in previous frame
+
 
         void pinch()
         {
             if (Input.GetButtonDown("Fire1")) // check that there are two touches
             {
-               
-                    manualZoom += zoomSpeed;
-                    Debug.Log("zooming in "+ manualZoom);
-                    PlayerPrefs.SetFloat("manualZoom", manualZoom);
-                    PlayerPrefs.Save();
-                    // Optionally clamp the zoom level, e.g., between a min and max value
-                    if(manualZoom > 1.7f)
-                    {
-                        manualZoom = 1;
-                    }
-                    manualZoom = Mathf.Clamp(manualZoom, 1f, 2f);
 
-                   
-                
+                manualzoom += zoomSpeed;
+                Debug.Log("zooming in " + manualzoom);
+                PlayerPrefs.SetFloat("manualzoom", manualzoom);
+                PlayerPrefs.Save();
+                // Optionally clamp the zoom level, e.g., between a min and max value
+                if (manualzoom > 1.7f)
+                {
+                    manualzoom = 1;
+                }
+                manualzoom = Mathf.Clamp(manualzoom, 1f, 2f);
+
+
+
                 UpdateSizes();
             }
         }
@@ -276,17 +273,17 @@ namespace TiltShift.Cardboard
                 return;
             }
 
-            _leftEyeMaterial.mainTexture  = _leftEyeRT;
+            _leftEyeMaterial.mainTexture = _leftEyeRT;
             _rightEyeMaterial.mainTexture = _rightEyeRT;
 
             var _transform = transform;
-            var position   = _transform.position;
-            var rotation   = _transform.rotation;
-            
+            var position = _transform.position;
+            var rotation = _transform.rotation;
+
             _leftEyeMaterial.SetPass(0);
-            
+
             Graphics.DrawMeshNow(_lensDistortion.GetDistortionMesh(CardboardEye.Left), position, rotation);
-            
+
             _rightEyeMaterial.SetPass(0);
             Graphics.DrawMeshNow(_lensDistortion.GetDistortionMesh(CardboardEye.Right), position, rotation);
         }
@@ -301,7 +298,7 @@ namespace TiltShift.Cardboard
         {
             MiddleCam.gameObject.SetActive(!MiddleCam.gameObject.activeSelf);
         }
-        
+
         private void Exit()
         {
             Application.Quit();
